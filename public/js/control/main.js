d30c1119263d304e8f4f2a1c16198e9d57312a0c
Sergey <info@magwai.ru>
2010-04-14 15:25:50 +0400

    mce fix

--- public/js/control/main.js
+++ public/js/control/main.js
@@ -334,7 +334,7 @@ c.implode = function(glue, pieces) {
 };
 
 c.submit = function(apply) {
-	if (typeof tinyMCE != 'undefined') tinyMCE.triggerSave();
+	//if (typeof tinyMCE != 'undefined') tinyMCE.triggerSave();
 	c.loading_start(true);
 	var i = $('#c_form .uploadifyQueueItem').not('#c_form .uploadifyQueueLoaded');
 	if (i.length == 0) c.sumbit_full(apply);
