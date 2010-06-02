659de47a4bb158f96ae69a06507bec72501f88ce
Sergey <info@magwai.ru>
2010-06-02 13:31:18 +0400

    txt fix

--- library/View/Helper/Txt.php
+++ library/View/Helper/Txt.php
@@ -8,15 +8,22 @@
  */
 
 class Zkernel_View_Helper_Txt extends Zend_View_Helper_Abstract  {
+	private $_data = null;
+
 	public function txt($key = null, $field = 'value') {
     	if ($key === null) return $this;
     	else {
-			$model = new Default_Model_Txt();
-			$ret = $model->fetchRow(array(
-   				'`key` = ?' => $key
-   			));
-   			$ret = $this->view->override()->overrideSingle($ret, 'txt');
-   			return $ret->$field;
+    		if ($this->_data === null) {
+    			$model = new Default_Model_Txt();
+    			$all = $model->fetchAll();
+    			if (count($all)) {
+    				$all = $this->view->override($all, 'txt');
+
+    				foreach ($all as $el) $this->_data[$el->key] = $el;
+    			}
+    			else $this->_data = array();
+    		}
+   			return @$this->_data[$key]->$field;
     	}
     }
 }
