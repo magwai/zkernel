08a88c9803e49966500489cc91722aeb5e5c9fb4
Sergey <info@magwai.ru>
2010-10-14 15:02:31 +0400

    twitter update fix, jscrollpane

--- library/Db/Model/Twitter.php
+++ library/Db/Model/Twitter.php
@@ -16,14 +16,21 @@ class Zkernel_Db_Model_Twitter {
     	$this->_twitter = Zend_Registry::get('Zkernel_Twitter');
     	if (isset($this->_twitter[$this->_name]['service'])) $this->_service = $this->_twitter[$this->_name]['service'];
 		else {
+			$consumer_key = isset($options['consumer_key']) ? $options['consumer_key'] : $this->_twitter['consumer_key'];
+			$consumer_secret = isset($options['consumer_secret']) ? $options['consumer_secret'] : $this->_twitter['consumer_secret'];
+			$callback_url = isset($options['callback_url']) ? $options['callback_url'] : $this->_twitter['callback_url'];
 			$token = isset($options['token']) ? $options['token'] : $this->_twitter['token'];
 			$token_secret = isset($options['token_secret']) ? $options['token_secret'] : $this->_twitter['token_secret'];
 			$at = new Zend_Oauth_Token_Access();
 			$at->setToken($token);
 			$at->setTokenSecret($token_secret);
-			$this->_service = new Zend_Service_Twitter(array(
+			$opt = array(
 			    'accessToken' => $at
-			));
+			);
+			if ($consumer_key) $opt['consumerKey'] = $consumer_key;
+			if ($consumer_secret) $opt['consumerSecret'] = $consumer_secret;
+			if ($callback_url) $opt['callbackUrl'] = $callback_url;
+			$this->_service = new Zend_Service_Twitter($opt);
 			$this->_twitter[$this->_name]['service'] = $this->_service;
 			Zend_Registry::set('Zkernel_Twitter', $this->_twitter);
 		}
