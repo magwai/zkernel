ea6c5a824977867630d3a8287ee7066a86836db7
Sergey <info@magwai.ru>
2010-11-03 17:36:51 +0300

    placeholder, quicksand add

--- library/View/Helper/Meta.php
+++ library/View/Helper/Meta.php
@@ -12,8 +12,8 @@ class Zkernel_View_Helper_Meta extends Zend_View_Helper_Abstract  {
 		$model = new Default_Model_Meta();
 		$meta = $model->fetchMatch(@$_SERVER['REQUEST_URI']);
 		if ($meta) {
-			if (@$meta['keywords']) $this->view->headMeta($this->view->escape($meta['keywords']), 'keywords');
-			if (@$meta['description']) $this->view->headMeta($this->view->escape($meta['description']), 'description');
+			if (@$meta['description']) $this->view->headMeta($this->view->escape($meta['description']), 'description', 'name', array(), 'SET');
+			if (@$meta['keywords']) $this->view->headMeta($this->view->escape($meta['keywords']), 'keywords', 'name', array(), 'SET');
 			if (@$meta['title']) $this->view->headTitle($this->view->escape($meta['title']), 'SET');
 		}
     }
