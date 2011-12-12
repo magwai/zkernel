00aa65aac90e5a730071c3cb443aeb3ddb2216b2
Сергей <m4a1f@sloboda.lo>
2011-12-12 15:37:21 +0400

    Action controller fix

--- library/Controller/Action.php
+++ library/Controller/Action.php
@@ -17,13 +17,16 @@ class Zkernel_Controller_Action extends Zend_Controller_Action {
 	public $model;
 
 	function init() {
+		$p = explode('/', trim(@$_SERVER['REQUEST_URI'], '/'));
 		$model = 'Default_Model_'.ucfirst($this->getRequest()->getControllerName());
 		if (@class_exists($model)) $this->model = new $model();
-		if (substr($this->getRequest()->getActionName(), 0, 3) == 'ctl') {
+		if (substr(@$p[1], 0, 3) == 'ctl') {
+			$pp = array();
+			for ($i = 2; $i < count($p); $i += 2) $pp[$p[$i]] = @$p[$i + 1];
 			$this->_helper->viewRenderer('control/router', null, true);
-			$this->view->controller = $this->getRequest()->getControllerName();
-			$this->view->action = $this->getRequest()->getActionName();
-			$this->view->param = $this->getRequest()->getParams();
+			$this->view->controller = $p[0];
+			$this->view->action = $p[1];
+			$this->view->param = $pp;
 			$this->view->post = $_POST;
 			$this->view->model = $this->model;
 			unset($this->view->param['controller']);unset($this->view->param['action']);unset($this->view->param['module']);
